9f54ffecd8c97d5f8219385ffd898e42a2d39f34
jgironUCF <joseph.giron@ucf.edu>
2020-04-15 16:40:00 -0400

    added custom college-style sheet and custom googlemap.js file

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ function my_theme_enqueue_styles() {
  
     $parent_style = 'Colleges-Theme-style';
  
+    wp_enqueue_script( 'googlemap', 'googlemap.js', false );
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/static/css/style.min.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/css/custom.css?'.rand(111,9999),
