ae17d024d587e72595a4f358fe7089ddb6033137
jgironUCF <joseph.giron@ucf.edu>
2020-04-21 11:48:56 -0400

    added function.php back to code to include custom.css

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+define( 'CONNECT', trailingslashit( get_stylesheet_directory() ) );
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'Colleges-Theme-style';
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/static/css/style.min.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/css/custom.css?'.rand(111,9999),
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+?>
