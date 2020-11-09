ef9eb0cd77eaa3de7d9d7ce639994d2e9d637e26
Joseph Giron <jgiron@ucf.edu>
2020-11-09 15:01:17 -0500

    fix syntax error

--- functions.php
+++ functions.php
@@ -18,12 +18,13 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'royalslider-custom',
         get_stylesheet_directory_uri() . '/css/royalslider/royalslider.css?'.rand(111,9999),
         array( $parent_style ),
-    wp_get_theme()->get('Version');
+        wp_get_theme()->get('Version')
+    );
 
     wp_enqueue_style( 'royalslider-default',
         get_stylesheet_directory_uri() . '/css/royalslider/skins/default/rs-default.css?'.rand(111,9999),
         array( $parent_style ),
-    wp_get_theme()->get('Version');
-);
+        wp_get_theme()->get('Version')
+    );
 }
 ?>
