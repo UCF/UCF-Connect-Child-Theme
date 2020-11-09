003a2070a5d18aa871fb1eeea85e217899bc8b57
Joseph Giron <jgiron@ucf.edu>
2020-11-09 15:12:39 -0500

    adding royalslider js to template

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 define( 'UCN_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_scripts' );
 function my_theme_enqueue_styles() {
  
     $parent_style = 'Colleges-Theme-style';
