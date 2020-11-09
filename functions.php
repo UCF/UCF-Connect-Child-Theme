7278a09344a5fde8eaf084af56049b46ff31f707
Joseph Giron <jgiron@ucf.edu>
2020-11-09 14:58:06 -0500

    adding more royalslider.css

--- functions.php
+++ functions.php
@@ -8,16 +8,22 @@ function my_theme_enqueue_styles() {
     $parent_style = 'Colleges-Theme-style';
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/static/css/style.min.css' );
+
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/css/custom.css?'.rand(111,9999),
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
 
-    wp_enqueue_style( 'royalslider',
-    get_stylesheet_directory_uri() . '/css/royalslider/skins/default/rs-default.css?'.rand(111,9999),
-    array( $parent_style ),
-    wp_get_theme()->get('Version')
+    wp_enqueue_style( 'royalslider-custom',
+        get_stylesheet_directory_uri() . '/css/royalslider/royalslider.css?'.rand(111,9999),
+        array( $parent_style ),
+    wp_get_theme()->get('Version');
+
+    wp_enqueue_style( 'royalslider-default',
+        get_stylesheet_directory_uri() . '/css/royalslider/skins/default/rs-default.css?'.rand(111,9999),
+        array( $parent_style ),
+    wp_get_theme()->get('Version');
 );
 }
 ?>
