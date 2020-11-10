077be387a9ea6372fa9843bbdb8463084f526268
Joseph Giron <jgiron@ucf.edu>
2020-11-10 10:46:03 -0500

    testing js file

--- functions.php
+++ functions.php
@@ -29,11 +29,10 @@ function my_theme_enqueue_styles() {
     );
 
     //https://github.com/UCF/UCF-Libraries-Theme/blob/master/functions.php
-    wp_register_script('royalslider-min-js', get_template_directory_uri() . '/royalslider/jquery.royalslider.min.js', false, null);
-    wp_enqueue_script('royalslider-min-js');
-    //wp_register_script('jquery.tablesorter.min', get_template_directory_uri() . '/js/jquery.tablesorter.min.js', 'jquery');
-    //wp_register_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", false, null);
+    //wp_register_script('royalslider-min-js', get_template_directory_uri() . '/royalslider/jquery.royalslider.min.js', false, null);
+    //wp_enqueue_script('royalslider-min-js');
+    wp_enqueue_script('royalslider-min-js', get_template_directory_uri() . '/royalslider/jquery.royalslider.min.js');
 
-    //wp_enqueue_script( 'royalslider-min-js', get_template_directory_uri() . '/css/royalslider/jquery.royalslider.min.js');
+
 }
 ?>
