e4475aa7c9a750c2dc8f6e1df900aff9ab4a02f5
Joseph Giron <jgiron@ucf.edu>
2020-12-04 16:52:36 -0500

    commented out autoHeight

--- royalslider/royalslider.js
+++ royalslider/royalslider.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($) {
     autoScaleSlider: true, 
     //autoScaleSliderWidth: 960,     
     //autoScaleSliderHeight: 850,
-    autoHeight:false,
+    //autoHeight:false,
 
     /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
     //imgWidth: 640,
